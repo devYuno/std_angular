232f3d3b3c2f01a369cc0434da415ca9582e6fc0
devYuno <annykessy20@gmail.com>
2026-03-26 17:27:52 -0300

    Login e register page

--- roteamento/src/app/features/login-page/login/login.component.ts
+++ roteamento/src/app/features/login-page/login/login.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  realEmail: string = "aaa";
+  realSenha: string = "123";
+
+
+  email: string = '';
+  senha: string = '';
+
+  onInput(event: any, campo: string) {
+    if (campo == "email") {
+      this.email = event.target.value;
+    } else if (campo == "senha") {
+      this.senha = event.target.value;
+    }
+  }
+
+  login() {
+    if (this.email == this.realEmail && this.senha == this.realSenha) {
+      
+    }
+  }
 }
