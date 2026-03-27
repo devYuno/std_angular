f74382edd2414b7b544b882e37acbe5dd09562d8
devYuno <annykessy20@gmail.com>
2026-03-27 16:48:37 -0300

    ehsrighdughrau

---
+++ RPlace/src/app/app.ts
@@ -0,0 +1,33 @@
+import { Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,
+            FormsModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('RPlace');
+  protected _date = signal(new Date());
+
+  protected _stringDate = computed(() => {
+    return `${this._date().getDate()}-${this._date().getMonth()+1}-${this._date().getFullYear()}`
+  })
+
+  ngOnInit() {
+    console.log(this.title())
+  }
+
+  addDay(add: boolean = true) {
+    this._date.update((oldValue) => {
+      let day = add ? oldValue.getDate() +1 : oldValue.getDate() -1;
+      let month = oldValue.getMonth();
+      let year = oldValue.getFullYear();
+
+      return new Date(year, month, day);
+    })
+  }
+}
